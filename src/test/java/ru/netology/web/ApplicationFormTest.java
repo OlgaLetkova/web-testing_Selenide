03c21cc7c4d34a00192281cda945900e8332cdde
Olga Letkova <olja.let52@gmail.com>
2021-01-16 22:22:13 +0300

    Added tests for validation's checking

--- src/test/java/ru/netology/web/ApplicationFormTest.java
+++ src/test/java/ru/netology/web/ApplicationFormTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static java.lang.Thread.sleep;
 
 public class ApplicationFormTest {
     @Test
@@ -18,5 +17,26 @@ public class ApplicationFormTest {
         form.$("[data-test-id='agreement']").click();
         form.$(".button").click();
         $("[data-test-id='order-success']").shouldHave(Condition.exactText("Ваша заявка успешно отправлена!"));
+    }
+
+    @Test
+    void shouldCheckValidationTest(){
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='name'] [type='text']").setValue("Ivan Petrov");
+        form.$("[data-test-id='phone'] [type='tel']").setValue("+79513265800");
+        form.$("[data-test-id='agreement']").click();
+        form.$(".button").click();
+        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldCheckOfSendingFormWithoutPhoneTest(){
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='name'] [type='text']").setValue("Александр Свифт");
+        form.$("[data-test-id='agreement']").click();
+        form.$(".button").click();
+        $("[data-test-id='phone'] .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 }
